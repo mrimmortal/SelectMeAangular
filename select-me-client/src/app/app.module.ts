66f38959d2556dfbd47c12bcf61d8c4f3f8e8a64
Tejas <tejasyesurkar0@gmail.com>
2021-05-26 07:29:53 +0530

    All mid part of gallery

--- select-me-client/src/app/app.module.ts
+++ select-me-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryViewComponent } from './gallery-view/gallery-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryViewComponent
   ],
   imports: [
     BrowserModule,
